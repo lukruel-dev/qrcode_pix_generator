5348aa228af248a0997fd2dd526da9a450eca0d6
lukruel-dev <lkbenq@gmail.com>
2026-03-24 22:57:19 -0300

    Chore: Improve gitignore and remove build artifacts from repo

--- .next/server/chunks/ssr/_next-internal_server_app_dashboard_keys_page_actions_0cw8c0a.js
+++
@@ -1,3 +0,0 @@
-module.exports=[97772,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_dashboard_keys_page_actions_0cw8c0a.js.map
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CiugiZ6/WeXZbyNHj3yeYxuqsaBi2s1RhFLRRtVNjdA=\"\n}"
